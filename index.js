8289ebe8f8c80fbbd089fd06461160de3bd69142
Satish Kumar <satishbgs441@gmail.com>
2021-05-28 21:52:00 +0530

    eval function not working

--- index.js
+++ index.js
@@ -4,27 +4,25 @@ console.log(buttons);
 
 buttons.map(button => {
   button.addEventListener('click',(e) => {
-    
     switch(e.target.innerText){
       case 'C':
         display.innerText=' ';
         break;
       case '←':
-        if(display.innerText){
-           display.innerText=display.innerText.slice(0,-1);
-        }
+        display.innerText=display.innerText.slice(0,-1);
         break;
       case '=':
         try {
           display.innerText=eval(display.innerText);
         } catch (error) {
-         display.innerText='ERROR!' ;
+          display.innerText="ERROR!";
         }
         
         break;
-      
       default:
-        display.innerText +=e.target.innerText;
+        display.innerText+=e.target.innerText;
     }
+   
+    
   });
 });
